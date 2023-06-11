db04f2acc82a2e4b5072fc642aba632d761dbc4a
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-06-11 18:50:40 -0500

    inicializacion del proyecto

---
+++ sistema-educativo/src/main/java/ec/edu/ups/sistemaeducativo/SistemaEducativoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.sistemaeducativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEducativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEducativoApplication.class, args);
+	}
+
+}
