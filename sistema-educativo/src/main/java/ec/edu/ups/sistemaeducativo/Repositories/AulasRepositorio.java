f020b2ab26365bccf15d9a87b1d384bcb4e4eb37
ChristianPaul97 <cportoviejo@est.ups.edu.ec>
2023-06-20 20:48:16 -0500

    2.0

---
+++ sistema-educativo/src/main/java/ec/edu/ups/sistemaeducativo/Repositories/AulasRepositorio.java
@@ -0,0 +1,15 @@
+package ec.edu.ups.sistemaeducativo.Repositories;
+
+import org.springframework.stereotype.Repository;
+
+import ec.edu.ups.sistemaeducativo.Models.Aulas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+@Repository
+public interface AulasRepositorio extends JpaRepository<Aulas, Long> {
+
+    Optional<Aulas> findAulatebyId(Long aulId);
+
+}
---
+++ sistema-educativo/src/main/java/ec/edu/ups/sistemaeducativo/Repositories/PeriodoLectivoRepositorio.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.sistemaeducativo.Repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.edu.ups.sistemaeducativo.Models.PeriodoLectivo;
+
+public interface PeriodoLectivoRepositorio extends JpaRepository<PeriodoLectivo, Long> {
+
+    Optional<PeriodoLectivo> findPeridiodoLectivobyId(Long plId);
+}
