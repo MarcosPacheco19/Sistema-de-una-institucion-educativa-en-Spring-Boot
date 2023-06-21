88620ab7e024206040102ace3350f2ab9dae0690
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-06-20 20:26:07 -0500

    cambios

--- sistema-educativo/src/test/java/ec/edu/ups/sistemaeducativo/SistemaEducativoApplicationTests.java
+++ sistema-educativo/src/test/java/ec/edu/ups/sistemaeducativo/SistemaEducativoApplicationTests.java
@@ -1,12 +1,9 @@
 package ec.edu.ups.sistemaeducativo;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class SistemaEducativoApplicationTests {
-	@Test
-	void contextLoads() {}
+
 }
