6de9afb6d39fae385da1164588f9bac56460e1a2
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-07-23 17:51:42 -0500

    Cors

---
+++ sistema-educativo/src/main/java/ec/edu/ups/sistemaeducativo/Config/CorsConfig.java
@@ -0,0 +1,19 @@
+package ec.edu.ups.sistemaeducativo.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
