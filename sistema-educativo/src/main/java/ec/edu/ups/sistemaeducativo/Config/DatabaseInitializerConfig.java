b7970f9d7c3a5a3d5b32440de57f67538c3ad5a9
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-07-25 23:47:50 -0500

    arreglos del login

--- sistema-educativo/src/main/java/ec/edu/ups/sistemaeducativo/Config/DatabaseInitializerConfig.java
+++ sistema-educativo/src/main/java/ec/edu/ups/sistemaeducativo/Config/DatabaseInitializerConfig.java
@@ -2,32 +2,27 @@ package ec.edu.ups.sistemaeducativo.Config;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class DatabaseInitializerConfig {
 
-    @Bean
-    public ServletContextInitializer initializer(DataSource dataSource) {
-        return new ServletContextInitializer() {
-            @Override
-            public void onStartup(ServletContext servletContext) throws ServletException {
-                try {
-                    ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-                    populator.addScript(new ClassPathResource("data.sql"));
-                    populator.execute(dataSource);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+    @Autowired
+    private DataSource dataSource;
 
-        };
+    @PostConstruct
+    public void initializer() {
+        try {
+            ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+            populator.addScript(new ClassPathResource("data.sql"));
+            populator.execute(dataSource);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
